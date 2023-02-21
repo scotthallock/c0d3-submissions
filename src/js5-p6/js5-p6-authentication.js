d8b43b1132b67fc099c39107a3ef7896605b6ad1
Scott Hallock <scott.hallock@gmail.com>
2023-02-20 23:50:21 -0600

    js5-6 auth router complete

--- src/js5-p6/js5-p6-authentication.js
+++ src/js5-p6/js5-p6-authentication.js
@@ -4,10 +4,8 @@ const path = require('path');
 const jsonParser = require('body-parser').json();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const dotenv = require('dotenv');
-const { appendFile } = require('fs');
-const { nextTick } = require('process');
 
+const SECRET = 'not_so_secret_am_i_?';
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/js5-p6/js5-p6.html'));
@@ -15,103 +13,96 @@ router.get('/', (req, res) => {
 
 const users = [];
 
-/* This is for TESTING PURPOSES ONLY */
+/* For testing purposes - send users array to client */
 router.get('/api/users', (req, res) => {
     res.json(users);
 });
 
-/* POST /api/users to create a new user */
+/* Create a new user */
 router.post('/api/users', jsonParser, async (req, res) => {
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
+    const { username, email, password, ...others } = req.body;
+    // const password = Buffer.from(req.body.password, 'base64'); // same as atob()
 
-    /* password cannot be blank and must be >5 characters */
+    /* Input validation */
     if (!password || password.length <= 5) {
-        return res.status(400).json({error: {message: 'password cannot be blank and must be at least 6 characters long'}});
+        return res.status(400).json({ "error": { "message":
+            "password field cannot be empty and must be base 64 encoded with more than 5 characters" } });
     }
-    /* username cannot be blank and must be alphanumeric */
     if (!username || !/^\w+$/.test(username)) {
-        return res.status(400).json({error: {message: 'username cannot be blank and can only contain letters, numbers, and "_"'}});
+        return res.status(400).json({ "error": { "message":
+            "username field cannot be blank and must contain alpha numeric characters only" } });
     }
-    /* username must be unique */
     if (users.some(e => e.username.toLowerCase() === username.toLowerCase())) {
-        return res.status(400).json({error: {message: 'username already taken'}});
+        return res.status(400).json({ "error": { "message":
+            "username is taken, please pick another" } });
     }
-    /* email must contain a @ symbol */
     if (!email || !email.includes('@')) {
-        return res.status(400).json({error: {message: 'email cannot be blank and must include the "@" character'}});
+        return res.status(400).json({ "error": { "message":
+            "email field cannot be blank and must be a valid email" } });
     }
-    /* email must be unique */
     if (users.some(e => e.email.toLowerCase() === email.toLowerCase())) {
-        return res.status(400).json({error: {message: 'there is already an account with that email'}});
+        return res.status(400).json({"error": {"message":
+            "there is already an account with that email"}});
     }
-    /* Users should be able to pass in any additional key/value data pairs in the body */
+
+    /* create user object, hash the password, and add a JWT */
     try {
         const salt = await bcrypt.genSalt();
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
         const user = {
-            username: req.body.username,
-            email: req.body.email,
-            password: hashedPassword,
-            // other key-value pairs
+            'username': username,
+            'email': email,
+            'password': hashedPassword,
+            ...others,
+            'jwt': jwt.sign({ username }, SECRET)
         };
         users.push(user);
-        return res.sendStatus(201);
+        return res.status(201).send(user);
     } catch (err) {
         console.error(err);
-        res.sendStatus(500);
+        res.send(500).json('Internal server error');
     }
 });
 
-/* POST /api/sessions to create a new session (aka login a user) */
-/* AUTHENTICATION - when the user successfully logs in using their 
-   credentials, a JSON Web Token will be returned. */
+/* Login a user */
 router.post('/api/sessions', jsonParser, async (req, res) => {
+    const { username, password } = req.body;
     /* username field can be either username or email */
-    /* password field must match the password for the user */
-
-    const user = users.find(e => e.username === req.body.username);
+    const user = users.find(e => {
+        return (e.username === username) ||
+               (e.email === username);
+    });
     if (!user) {
         return res.status(400).json('User not found');
     }
     try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            // credentials are correct, create a JWT
-            return res.json({user, jwt: jwt.sign(user, 'secret password')});
+        if (await bcrypt.compare(password, user.password)) {
+            /* Credentials are correct, create a new JWT and send back the user */
+            user.jwt = jwt.sign({ username }, SECRET);
+            return res.json(user);
         }
         res.status(400).json('Wrong credentials');
     } catch (err) {
         console.error(err);
         res.status(500).json('Internal server error');
     }
-
 });
 
-/* GET /api/sessions to get the currently logged in user */
-/* AUTHORIZATION - */
+/* Get currently logged in user */
 router.get('/api/sessions', (req, res) => {
     /* The value for the 'Authorization' header should be 'Bearer [jwt]' */
     const authHeader = req.get('Authorization');
     const token = authHeader && authHeader.split(' ')[1];
-    console.log(token);
     if (!token) {
-        return res.status(401).json('Unauthorized: "Authorization" header lacks valid token or is incorrectly formatted');
+        return res.status(401).json('Unauthorized - no token provided');
     }
-    /* Asynchronous verify token */
-    jwt.verify(token, 'secret password', (err, user) => {
-        if (err) return res.status(403).json('Token verification bad');
+    try {
+        const decoded = jwt.verify(token, SECRET);
+        const user = users.find(e => e.username === decoded.username);
         return res.json(user);
-    });
+    } catch (err) {
+        return res.status(400).json({ error: "Invalid token" });
+    }
 });
-
-/* Middleware - authenticate token */
-/* Once the user is logged in, each subsequent request will include the JWT,
-   allowing the user to access routes, services, and resources that are
-   permitted with that token. */
-
-
-
-
 
 module.exports = router;
