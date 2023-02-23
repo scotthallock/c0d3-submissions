0b4a3d6a322c24d5c6824f554826c749edc38803
Scott Hallock <scott.hallock@gmail.com>
2023-02-22 23:55:15 -0600

    add image processing function

--- src/js5-p7/js5-p7-image-text-extraction.js
+++ src/js5-p7/js5-p7-image-text-extraction.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const multer = require('multer');
-const { createWorker, PSM } = require('tesseract.js');
+const { createWorker, PSM, imageType } = require('tesseract.js');
 const { v4: uuidv4 } = require('uuid');
 
 /* Where user uploads will be stored */
@@ -30,41 +30,54 @@ router.get('/', (req, res) => {
 router.post('/api/assets', upload.array('userFiles'), async (req, res) => {
     /* What about rotation pre-processing? */
     console.log('/api/assets ouch ', Date.now);
+
     const images = req.files;
     const jobId = uuidv4(); // job id
-    // process(images, jobId);
+
+    /* add initial data to job */
+    jobs[jobId] = images.reduce((acc, image) => {
+      acc.push({
+        filename: image.filename,
+        size: image.size,
+        status: 'processing', // processing | done
+        ocr_data: null
+      })
+      return acc;
+    }, []);
+
+    process(images, jobId);
     res.status(202).json({ images, jobId }); // this will be a link to the jobs
     // the server should then make a request once per second to jobs/:jobId
 });
 
-const jobs = []; // id1: {}, id2: {}
+const jobs = {}; // id1: {}, id2: {}
 
 router.get('/jobs/:id', (req, res) => {
   console.log('/jobs/:id ouch ', Date.now());
-  console.log(req.params.id);
-  res.end();
+  res.json();
 });
 
-const process = async (images) => {
-    /* for each image uploaded, start a  */
-    // let dataTSV;
-    // try {
-    //   const worker = await createWorker();
-    //   await worker.loadLanguage('eng');
-    //   await worker.initialize('eng');
-    //   await worker.setParameters({
-    //     tessedit_char_whitelist: ' 1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    //     tessedit_pageseg_mode: PSM.SPARSE_TEXT, // Find as much text as possible in no particular order.
-    //   });
-    //   const filepath = path.join(uploadsDirectory, req.file.originalname);
-    //   const { data: { tsv } } = await worker.recognize(filepath);
-    //   dataTSV = tsv;
-    //   await worker.terminate();
-    // } catch (err) {
-    //   res.status(400).json({error: {message: 'Error during OCR: ', err}});
-    // }
-
-    // return res.json(parseTSV(dataTSV));
+const process = (images, jobId) => {
+    /* use tesseract to extract text from each image */
+    images.forEach(async (image, i) => {
+      try {
+        const worker = await createWorker();
+        await worker.loadLanguage('eng');
+        await worker.initialize('eng');
+        await worker.setParameters({
+          tessedit_char_whitelist: ' 1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+          tessedit_pageseg_mode: PSM.SPARSE_TEXT, // Find as much text as possible in no particular order.
+        });
+        const filepath = path.join(uploadsDirectory, image.filename);
+        const { data: { tsv } } = await worker.recognize(filepath);
+        /* update data and status of image in job */
+        jobs[jobId][i].status = 'done';
+        jobs[jobId][i].ocr_data = parseTSV(tsv);
+        await worker.terminate();
+      } catch (err) {
+        res.status(400).json({error: {message: 'Error during OCR: ', err}});
+      }
+    })
 };
 
 const parseTSV = (tsv) => {
