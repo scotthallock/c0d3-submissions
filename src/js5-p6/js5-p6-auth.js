52fdc74f26aef9a9a3f5f0f04847b24b666e411d
Scott Hallock <scott.hallock@gmail.com>
2023-02-28 00:30:21 -0600

    server no longer stores jwts, passwords not sent back to client

--- src/js5-p6/js5-p6-auth.js
+++ src/js5-p6/js5-p6-auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
 const path = require('path');
 const jsonParser = require('body-parser').json();
 const bcrypt = require('bcrypt');
@@ -7,44 +8,54 @@ const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = 'not_very_secret_am_i_?';
 
-/* Array of all users */
-const users = [];
+/* Storage for all users */
+const users = {};
 
 /* For test purposes - add some users to the array */
 (async () => {
-    let salt = await bcrypt.genSalt();
-    users.push({
+    const salt = await bcrypt.genSalt();
+    users['fishing_freak'] = {
         username: 'fishing_freak',
         email: 'fishingislife@gmail.com',
         password: await bcrypt.hash('ilovefishing123', salt)
-    });
-    salt = await bcrypt.genSalt();
-    users.push({
+    };
+    users['chicagoDawg'] = {
         username: 'chicagoDawg',
         email: 'doggydog@hotmail.com',
         password: await bcrypt.hash('must4rd&rel1sh', salt)
-    });
-
+    };
 })();
 
 /* Check user signup fields are valid */
 const validateSignUp = async (username, email, password) => {
-    if (!password || password.length <= 5) {
-        return 'password field cannot be empty and must have 6 or more characters';
+    if (password.length <= 5) {
+        return 'Password must have 6 or more characters';
     }
-    if (!username || !/^\w+$/.test(username)) {
-        return 'username field cannot be blank and must contain alpha numeric characters only';
+    if (!(/^\w+$/.test(username))) {
+        return 'Username field must contain alphanumeric characters only';
     }
-    if (users.some(e => e.username.toLowerCase() === username.toLowerCase())) {
-        return 'username is taken, please pick another';
+    if (Object.values(users).some(u => u.username.toLowerCase() === username.toLowerCase())) {
+        return 'Username is taken, please pick another';
     }
-    if (!email || !email.includes('@')) {
-        return 'email field cannot be blank and must contain a \'@\' character';
+    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+        return 'Email must be properly formatted';
     }
-    if (users.some(e => e.email.toLowerCase() === email.toLowerCase())) {
-        return 'there is already an account with that email';
+    if (Object.values(users).some(u => u.email.toLowerCase() === email.toLowerCase())) {
+        return 'There is already an account with that email address';
     }
-    return null;
+    return undefined; // nothing wrong with signup fields
+};
+
+const findUserByUsernameOrEmail = (usernameOrEmail) => {
+    if (users[usernameOrEmail]) return users[usernameOrEmail];
+    const user = Object.values(users).find(u => u.email === usernameOrEmail);
+    console.log({user});
+    if (user) return user;
+    return undefined; // user not found
+};
+
+const signToken = (payload) => {
+    return jwt.sign({ payload }, JWT_SECRET, {expiresIn: '30s'});
 };
 
 /********************
@@ -56,101 +67,95 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/js5-p6/js5-p6.html'));
 });
 
+
 /* Allow Cross-Origin Resource Sharing (CORS) for /auth/api/* */
-router.options('/api/*', (req, res) => {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Credentials'
-    );
-    res.send('ok');
-  })
+router.use('/api/*', cors());
 
 
 /* For demo purposes - send users array to client */
 router.get('/api/users', (req, res) => {
-    res.json(users);
+    const usersWithPasswordsRemoved = Object.keys(users).reduce((acc, username) => {
+        const { ...copy } = users[username];
+        delete copy.password;
+        acc[username] = copy;
+        return acc;
+    }, {});
+    res.json(usersWithPasswordsRemoved);
 });
+
 
 /* Create a new user */
 router.post('/api/users', jsonParser, async (req, res) => {
     const { username, email, password, ...others } = req.body;
-    if (typeof password !== 'string') {
-        return res.status(400).json({ error: { message:
-            'password missing in the request body' } });
+    if (!username || !email || !password) {
+        return res.status(400).json({ error: { message: 'Must include username, email, and password' } });
     }
     const decodedPassword = Buffer.from(password, 'base64').toString('ascii'); // atob()
 
-    /* Input validation */
     const errorMessage = await validateSignUp(username, email, decodedPassword);
     if (errorMessage) return res.status(400).json({ error: { message: errorMessage } });
 
-    /* create user object, hash the password, and add a JWT */
     try {
         const salt = await bcrypt.genSalt();
         const hashedPassword = await bcrypt.hash(decodedPassword, salt);
-        const user = {
-            'username': username,
-            'email': email,
-            'password': hashedPassword,
-            ...others,
-            'jwt': jwt.sign({ username }, JWT_SECRET, {expiresIn: '30s'})
+        users[username] = {
+            username,
+            email,
+            password: hashedPassword,
+            ...others
         };
-        users.push(user);
-        return res.status(201).json(user);
+        return res.status(201).json({
+            username,
+            email,
+            jwt: signToken(username)
+        });
     } catch (err) {
-        console.error(err);
-        return res.send(500).json('Internal server error');
+        return res.send(500).json({ error: { message: err.message } });
     }
 });
-
 
 /* Login a user */
 router.post('/api/sessions', jsonParser, async (req, res) => {
     const { username, password } = req.body;
-    const decodedPassword = Buffer.from(password, 'base64').toString('ascii'); // atob()
-
-    /* allow user to login with username or email in the username field*/
-    const user = users.find(e => {
-        return (e.username === username) || (e.email === username);
-    });
-    if (!user) {
-        return res.status(400).json({'error': {'message':
-            'User not found'} });
+    if (!username || !password) {
+        return res.status(400).json({ error: { message: 'Must include username and password' } });
     }
+
+    const user = findUserByUsernameOrEmail(username);
+    if (!user) return res.status(400).json({ error: { message: 'User not found' } });
+
+    const decodedPassword = Buffer.from(password, 'base64').toString('ascii'); // atob()
 
     const passwordIsCorrect = await bcrypt.compare(decodedPassword, user.password);
     if (passwordIsCorrect) {
-        /* Create a new JWT for the user */
-        user.jwt = jwt.sign(
-            { 'username': user.username },
-            JWT_SECRET,
-            {expiresIn: '30s'}
-        );
-        return res.json(user);
+        const { username, email } = user;
+        return res.json({
+            username,
+            email,
+            jwt: signToken(username)
+        });
     }
-    return res.status(400).json({'error': {'message':
-        'Wrong credentials'} });
+    return res.status(400).json({error: {message: 'Wrong credentials'} });
 });
+
 
 /* Get currently logged in user */
 router.get('/api/sessions', (req, res) => {
     /* The value for the 'Authorization' header should be 'Bearer [jwt]' */
     const authHeader = req.get('Authorization');
     const token = authHeader && authHeader.split(' ')[1];
-    if (!token) {
-        return res.status(401).json({'error': {'message':
-            'Unauthorized - no token provided'} });
-    }
+    if (!token) return res.status(401).json({error: {message: 'Unauthorized - no token provided'} });
+
     try {
-        const decoded = jwt.verify(token, JWT_SECRET);
-        const user = users.find(e => e.username === decoded.username);
-        return res.json(user);
+        const decoded = jwt.verify(token, JWT_SECRET); // will throw error if invalid token
+        const { username, email } = users[decoded.payload];
+        return res.json({
+            username,
+            email,
+            jwt: token
+        });
     } catch (err) {
-        /* token is wrong or expired */
-        return res.status(403).json({'error': {'message': err.message} });
+        return res.status(403).json({error: {message: err.message} });
     }
 });
 
