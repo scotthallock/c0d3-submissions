def1102b5ebeebd3d5b5190d6363a6c18c4b8225
Scott Hallock <scott.hallock@gmail.com>
2023-02-28 14:01:30 -0600

    final formatting

--- src/js5-p8/js5-p8-selfie-queen.js
+++ src/js5-p8/js5-p8-selfie-queen.js
@@ -12,21 +12,6 @@ const iconsDirectory = path.join(__dirname, '../../public/js5-p8/icons');
 /* Create the /uploads folder if it does not exist */
 if (!fs.existsSync(uploadsDirectory)) fs.mkdirSync(uploadsDirectory);
 
-/* Set max size of selfie request body - default size is too small */
-router.use(bodyParser.json({limit: '10mb'}));
-
-router.use('/uploads', express.static(uploadsDirectory));
-router.use('/icons', express.static(iconsDirectory));
-
-/* Using timeago.min.js script in the html <script> */
-router.get('/timeago.js', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../node_modules/timeago.js/dist/timeago.min.js'));
-});
-
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/js5-p8/js5-p8.html'));
-});
-
 /* Initialize selfie metadata object */
 const selfies = {
     '_animal-selfie-1.png': {
@@ -39,36 +24,6 @@ const selfies = {
     }
 };
 
-/* User submits a selfie */
-router.post('/api/uploads', jsonParser, async (req, res) => {
-    console.log('/api/uploads ouch ', Date.now());
-    console.log('the emoji is ', req.body.emoji);
-    if (!req.body.selfie || !req.body.emoji) {
-        res.status(400).json({error: {message: 'Request is missing a selfie or emoji'}});
-    }
-    const time = Date.now();
-    const filename = time + '_' + uuidv4() + '.png';
-    const filepath = uploadsDirectory + '/' + filename;
-
-    try {
-        await fs.promises.writeFile(filepath, req.body.selfie, 'base64')
-        selfies[filename] = {
-            timestamp: time,
-            emoji: req.body.emoji
-        };
-        res.status(201).json(selfies[filename]);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({error: {message: 'Error while saving seflie file to server'}});
-    }
-});
-
-/* Send all selfies in the /uploads folder */
-router.get('/api/uploads', (req, res) => {
-    console.log('/api/uploads ouch ', Date.now());
-    res.json(selfies);
-});
-
 /* Delete old selfies from server */
 const deleteOldSelfies = async () => {
     try {
@@ -76,21 +31,67 @@ const deleteOldSelfies = async () => {
         files.forEach(async (filename) => {
             const filepath = uploadsDirectory + '/' + filename;
             const stats = await fs.promises.stat(filepath);
-            if (filename.startsWith('_animal')) { // don't delete the example animal selfies
+            if (filename.startsWith('_animal')) { // don't delete the 2 cat selfies
                 return;
             }
             if (Date.now() - stats.mtimeMs > 1000 * 60 * 60 * 24) { // 24 hours
                 await fs.promises.unlink(filepath); // delete the file
                 delete selfies[filename]; // delete the metadata
-                console.log(`Deleted ${file} from /uploads folder at ${new Date()}`);
+                console.log(`Deleted ${filename} from /uploads folder at ${new Date()}`);
             }
         });
     } catch (err) {
         console.error(err);
     }
-    /* Recursively call this function every 30 minutes */
-    setTimeout(deleteOldSelfies, 1000 * 60 * 30);
+    setTimeout(deleteOldSelfies, 1000 * 60 * 30); // 30 minutes
 };
 deleteOldSelfies();
+
+/* Set allowable size of POST request body - default size is too small */
+router.use(bodyParser.json({limit: '10mb'}));
+
+router.use('/uploads', express.static(uploadsDirectory));
+router.use('/icons', express.static(iconsDirectory));
+
+/* External style sheet */
+router.get('/js5-p8-styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/js5-p8/js5-p8-styles.css'));
+});
+
+/* Using timeago.min.js script in the html <script> */
+router.get('/timeago.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../node_modules/timeago.js/dist/timeago.min.js'));
+});
+
+/* Serve /selfie-queen page */
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/js5-p8/js5-p8.html'));
+});
+
+/* Get all selfies */
+router.get('/api/uploads', (req, res) => {
+    res.json(selfies);
+});
+
+/* Create a new selfie */
+router.post('/api/uploads', jsonParser, async (req, res) => {
+    if (!req.body.selfie || !req.body.emoji) {
+        return res.status(400).json({error: {message: 'Request is missing a selfie or emoji'}});
+    }
+    const time = Date.now();
+    const filename = time + '_' + uuidv4() + '.png';
+    const filepath = uploadsDirectory + '/' + filename;
+
+    try {
+        await fs.promises.writeFile(filepath, req.body.selfie, 'base64');
+        selfies[filename] = {
+            timestamp: time,
+            emoji: req.body.emoji
+        };
+        return res.status(201).json(selfies[filename]);
+    } catch (err) {
+        return res.status(500).json({error: {message: 'Error while saving seflie file to server'}});
+    }
+});
 
 module.exports = router;
