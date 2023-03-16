db92bad184a9102d095f693030db5dc52980b4c8
Scott Hallock <scott.hallock@gmail.com>
2023-03-16 01:46:58 -0500

    pokemon searching, selecting, and login

--- public/js6-p4/PokemonClassesApp.js
+++ public/js6-p4/PokemonClassesApp.js
@@ -16,18 +16,133 @@ const sendQuery = async (query) => {
       .then(r => r.data);
 };
 
-function PokemonClassesApp() {
+const debounce = (fn, time) => {
+  let timeout;
+  return () => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+          fn();
+      }, time);
+  };
+};
 
-  useEffect(() => {
-    (async () => {
-      const { lessons } = await sendQuery(`{lessons {title}}`);
-      console.log(lessons);
-    })();
-  }); 
+
+
+function PokemonSelection({name, image, onLogin}) {
+  return (
+    <div className="selectedSection">
+      <h1>{name}</h1>
+      <img src={image} />
+      <button onClick={() => onLogin(name)}>Login</button>
+    </div>
+  );
+};
+
+
+function PokemonSuggestions({searchResults, searchBoxValue, onLoadPokemon}) {
+  const matches = searchResults.map((e, i) => {
+    const pokemonName = e.name;
+
+    const result = pokemonName.replace(
+      searchBoxValue,
+      `<span class="match">${searchBoxValue}</span>`
+    );
   
+    return (
+      <h3
+        key={i}
+        dangerouslySetInnerHTML={{ __html: result}}
+        onClick={() => onLoadPokemon(pokemonName)}
+      />
+    );
+  });
+
+  return <div className="suggestions">{matches}</div>;
+}
+
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [loadedPokemon, setLoadedPokemon] = useState(null);
+
+  /* Helper functions **************************/
+  const searchPokemon = (str) => {
+    (debounce(() => {
+      sendQuery(`{
+        search(str: "${str}") {name}
+      }`)
+        .then(result => {
+          setSearchResults(result.search)
+        })
+        .catch(console.error);
+    }, 500))();
+  };
+
+  const getPokemon = (name) => {
+    sendQuery(`{
+      getPokemon(str: "${name}") {name, image}
+    }`)
+      .then(result => {
+        setLoadedPokemon(result.getPokemon)
+      })
+      .catch(console.error);
+  };
+
+  const loginPokemon = (name) => {
+    sendQuery(`{
+      login (pokemon: "${name}") {name}
+    }`)
+      .then(result => {
+        console.log("LOGGED IN!")
+        console.log(result);
+        window.location.reload();
+      })
+  };
+
+  /* Event Handlers ****************************/
+  const handleSearch = (e) => {
+    setLoadedPokemon(null);
+    const str = e.currentTarget.value;
+    setSearch(str);
+    searchPokemon(str);
+  };
+
+  const handleLoadPokemon = (name) => {
+    setSearchResults([]);
+    getPokemon(name);
+  };
+
+  const handleLogin = (name) => {
+    console.log('logging in...')
+    loginPokemon(name);
+  };
 
   return (
-    <h1>Waddup gamers</h1>
+    <div>
+      <h1>Pokemon Search</h1>
+      <input
+        className="searchBox"
+        type="text"
+        onChange={handleSearch}
+        value={search}
+        />
+      {
+        loadedPokemon
+        ? 
+        <PokemonSelection
+          name={loadedPokemon.name}
+          image={loadedPokemon.image}
+          onLogin={handleLogin}
+        />
+        :
+        <PokemonSuggestions
+          searchBoxValue={search}
+          searchResults={searchResults}
+          onLoadPokemon={handleLoadPokemon}
+        />
+      }
+    </div>
   );
 }
 
@@ -35,4 +150,4 @@ function PokemonClassesApp() {
 
 const domContainer = document.querySelector("#root");
 const root = ReactDOM.createRoot(domContainer);
-root.render(<PokemonClassesApp />);
+root.render(<App />);
