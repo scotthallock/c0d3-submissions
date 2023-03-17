e31eeacc85db4096036f7540afdc8db7ba72a6c9
Scott Hallock <scott.hallock@gmail.com>
2023-03-17 01:01:56 -0500

    webpack built js6-4

--- src/js6-p4/js6-p4-pokemon-classes.js
+++ src/js6-p4/js6-p4-pokemon-classes.js
@@ -5,11 +5,11 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const router = express.Router();
 
-
+/* Serving webpack'ed file in /dist folder */
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/js6-p4/index.html'));
+  res.sendFile(path.join(__dirname, '../../src/js6-p4/dist/index.html'));
 });
 
-router.use('/', express.static(path.join(__dirname, '../../public/js6-p4')));
+router.use('/', express.static(path.join(__dirname, '../../src/js6-p4/dist/')));
 
 export default router;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,44 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-module.exports = {
+/* Change these variables based on which challenge we want to bundle with Webpack */
+const ENTRY_FILE = './src/js6-p4/src/index.js';
+const ASSET_PATH = '/pokemon-classes';
+const OUTPUT_PATH = path.join(__dirname, '/src/js6-p4/dist');
+const TEMPLATE_FILE = './src/js6-p4/src/index.html';
+
+export default {
+  
+  entry: ENTRY_FILE,
+
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'index.bundle.js',
+    publicPath: ASSET_PATH,
+    path: OUTPUT_PATH,
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: TEMPLATE_FILE
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
   }
+
 };
